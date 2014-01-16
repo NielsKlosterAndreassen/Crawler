6755903d7f89043d43c5b167b19f5f27ee6eface
Niels Kloster Andreassen <nika@telenor.dk>
2014-01-16 14:00:51 +0100

    Initial empty implementation

---
+++ Crawler/Program.fs
@@ -0,0 +1,18 @@
+﻿module Crawler
+
+    let requestPage url =
+        ""
+
+    let getLinks url =
+        let html = requestPage(url)
+        ""
+
+    let crawlPage urls pagesVisted =
+        pagesVisited
+
+    [<EntryPoint>]
+    let main argv =
+        let urls = Seq.choose (fun x -> new Url(x))argv
+        let result = crawlPage(argv)
+        printfn "%A" argv
+        0 // return an integer exit code
